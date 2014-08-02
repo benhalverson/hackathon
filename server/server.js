2d60f5dda62eeaf78b295084b7c35798a3c9ea18
Ben Halverson <benhalverson33@gmail.com>
2014-08-01 20:58:10 -0700

    changed mongo info

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'hackathon.pro');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
