618efb463e6aa0095ad4b05bb3e4100f4d2d9c82
Ben Halverson <benhalverson33@gmail.com>
2014-08-01 21:43:26 -0700

    mongodb issues

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var bodyParser = require("body-parser");
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '../public'));
+app.use(express.static(__dirname + './public'));
 
 //function handleCors(req, res, callback) {
 //
