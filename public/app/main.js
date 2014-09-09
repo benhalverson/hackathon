7e5fde8133a147cb0c7c246ce00390cac741a202
AJ Funk <aj.funk@onmego.com>
2014-09-08 19:15:25 -0700

    Form validations

--- public/app/main.js
+++ public/app/main.js
@@ -33,9 +33,99 @@ hackathonsite.controller("AppCtrl", function ($http, $scope) {
         //TODO: reset fields after adding something to the database
         $http.post(url + "/add", {name:newListing}).success(function () {
             console.log("posting");
-            loadOrder();
+            
         })
     };
+
+    app.refresh = function () {
+        $('#name').val('');
+        $('#description').val('');
+        $('#city').val('');
+        $('#state').val('');
+        $('#zipcode').val('');
+        $('#logoURL').val('');
+        $('#website').val('');
+        $('#date').val('');
+        $('#type').val('');
+        $('#api').val('');
+        $('#price').val('');
+        $('#prize').val('');
+        $('#duration').val('');
+
+    };
+
+    $scope.check = function() {
+        var name =  $('#name').val();
+        var description =  $('#description').val();
+        var city =  $('#city').val();
+        var state =  $('#state').val();
+        var zipcode =  $('#zipcode').val();
+        var logoURL =  $('#logoURL').val();
+        var website =  $('#website').val();
+        var date =  $('#date').val();
+        var type =  $('#type').val();
+        var api =  $('#api').val();
+        var price =  $('#price').val();
+        var prize  =  $('#prize').val();
+        var duration =  $('#duration').val();
+
+        if(!name){
+            alert('Name Required');
+            return;
+        }
+        if(!description){
+            alert('Description Required')
+            return;
+        }
+        if(!city){
+            alert('City Required')
+            return;
+        }
+        if(!state){
+            alert('State Required')
+            return;
+        }
+        if(!zipcode){
+            alert('Zipcode Required')
+            return;
+        }
+        if(!logoURL){
+            alert('Logo URL Required')
+            return;
+        }
+        if(!website){
+            alert('Website Required')
+            return;
+        }
+        if(!date){
+            alert('Date Required')
+            return;
+        }
+        if(!type){
+            alert('Type Required')
+            return;
+        }
+        if(!api){
+            alert('API Required')
+            return;
+        }
+        if(!price){
+            alert('Price Required')
+            return;
+        }
+        if(!prize){
+            alert('Prize Required')
+            return;
+        }
+        if(!duration){
+            alert('Duration Required')
+            return;
+        }
+        
+        app.saveListing();
+        app.refresh();
+        
+    }
 
     app.removeItem = function () {
         $http.post(url + "/remove/:_id").success(function () {
