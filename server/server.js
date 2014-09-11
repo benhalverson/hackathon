b0ed9bd2b3ce5d3c3efc906224edaaaf6a8a0e3d
AJ Funk <aj.funk@onmego.com>
2014-09-10 21:14:27 -0700

    deletion update

--- server/server.js
+++ server/server.js
@@ -146,7 +146,7 @@ app.put('/update/:id', function(req, res){
 app.post("/remove/:id", function (req, res) {
     console.log("ready to remove");
     var id = req.route.params.id;
-    Listing.remove( {"_id": ObjectId("5410f1aa5e605d0200d86b7d")} );
+    Listing.remove( {"_id": 5410f1aa5e605d0200d86b7d} );
 
 });
 
