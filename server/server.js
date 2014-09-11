9962f866806dda05fd0d8969ffe83cb8b7e4f1cf
AJ Funk <aj.funk@onmego.com>
2014-09-10 21:47:23 -0700

    Deletion Testing

--- server/server.js
+++ server/server.js
@@ -148,7 +148,7 @@ app.put('/update/:id', function(req, res){
 //     res.send();
 // });
 
-app.put('/remove/:id?', function(req, res){
+app.get('/remove/:id?', function(req, res){
 
     var id = req.route.params.id;
     res.send("HIT REMOVAL ENDPOINT");
